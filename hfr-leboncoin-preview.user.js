3c319c916f2942b00f377edb31b72c64f5d0a201
Orken <subotm@free.fr>
2016-03-25 22:47:45 +0100

    Considérations graphiques :o

--- hfr-leboncoin-preview.user.js
+++ hfr-leboncoin-preview.user.js
@@ -1,6 +1,6 @@
 // ==UserScript== 
 // @name [HFR] Leboncoin preview 
-// @version 0.1.5
+// @version 0.1.6
 // @namespace http://lbc2rss.superfetatoire.com/ 
 // @description Permet de voir une preview des annonces leboncoin, inspiré de [HFR] Image quote preview 
 // @updateURL https://raw.githubusercontent.com/Orken/HFR-Leboncoin-preview/master/hfr-leboncoin-preview.user.js
@@ -10,14 +10,18 @@
 // @homepage https://github.com/Orken/HFR-Leboncoin-preview
 // @author Orken | Mr Marron Derriere
 // @icon http://lbc2rss.superfetatoire.com/webroot/img/icon.png
-// @grant GM_xmlhttpRequest 
+// @grant GM_xmlhttpRequest
+// @grant GM_addStyle
 // ==/UserScript== 
- 
-function testLinkToLBC(link) { 
+
+var css = '#loader-wrapper{position:relative;top:10px;left:0;width:100%;height:100%;z-index:1000}#loader-wrapper p{position:absolute;text-align:center;left:0;right:0;color:#666;line-height:30px;top:50%;margin-top:-15px;font-size:30px}#loader{display:block;position:relative;left:50%;top:50%;width:150px;height:150px;margin:0 0 0 -75px;border-radius:50%;border:3px solid transparent;border-top-color:#f56b2a;-webkit-animation:spin 1.5s linear infinite;animation:spin 1.5s linear infinite}#loader:before{content:"";position:absolute;top:5px;left:5px;right:5px;bottom:5px;border-radius:50%;border:3px solid transparent;border-top-color:#4183d7;-webkit-animation:spin 2s linear infinite;animation:spin 2s linear infinite}#loader:after{content:"";position:absolute;top:15px;left:15px;right:15px;bottom:15px;border-radius:50%;border:3px solid transparent;border-top-color:#ccc;-webkit-animation:spin 1s linear infinite;animation:spin 1s linear infinite}@-webkit-keyframes spin{0%{-webkit-transform:rotate(0deg);-ms-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(360deg);-ms-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes spin{0%{-webkit-transform:rotate(0deg);-ms-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(360deg);-ms-transform:rotate(360deg);transform:rotate(360deg)}}';
+var links;
+
+var testLinkToLBC = function (link) { 
     return link.href.match(/www.leboncoin.fr/i); 
 } 
  
-function loading(element) { 
+var loading = function (element) { 
     var dyn = ""; 
     return  setInterval(function() { 
         dyn+= "."; 
@@ -25,9 +29,6 @@ function loading(element) {
         element.innerHTML ="Chargement en cours" + dyn; 
     },500); 
 } 
-
-var filter   = Array.prototype.filter;
-var links = filter.call( document.querySelectorAll('#mesdiscussions a.cLink') , testLinkToLBC );
 
 var createRow = function (titre, content) {
     var row = document.createElement('tr');
@@ -60,7 +61,7 @@ var generateGallery = function (thumbs) {
         for (i=0;i<length;i++) { 
             var img = new Image();
             img.src = thumbs[i];
-            img.style.maxHeight = '300px';
+            img.style.maxHeight = '120px';
             img.style.maxWidth = (width / Math.min(length,3) ) + 'px';
             img.style.margin = '3px';
             gallery.appendChild(img);
@@ -98,26 +99,33 @@ var display = function (titre, description, thumbs, price, address) {
     }
     return content;
 };
- 
-links.filter(testLinkToLBC).forEach(function(link) { 
+
+GM_addStyle(css);
+
+links = Array.prototype.filter.call( document.querySelectorAll('#mesdiscussions a.cLink') , testLinkToLBC );
+links.forEach(function(link) {
     var container; 
     var loadingText; 
     var img; 
     var bePatient; 
     
     link.addEventListener('mouseover',function() { 
-        loadingText = document.createElement('p'); 
-        loadingText.style.marginLeft = '50px'; 
-        loadingText.style.fontFamily = 'arial,sans-serif'; 
-        loadingText.style.fontWeight = 'bold'; 
-        loadingText.style.textAlign = 'center'; 
-        loadingText.innerHTML = "Chargement en cours..."; 
-        bePatient = loading(loadingText); 
+        
+        loadingText = document.createElement('div');
+        loadingText.id = 'loader-wrapper';
+        var loader = document.createElement('div');
+        loader.id = 'loader';
+        var loadinginprogress = document.createElement('p');
+        loadinginprogress.innerHTML = 'Chargement en cours';
+        loadingText.appendChild(loadinginprogress);
+        loadingText.appendChild(loader);
+        bePatient = loading(loadinginprogress); 
 
         container = document.createElement('div'); 
         container.style.position = "absolute"; 
         container.style.background = "#fff"; 
-        container.style.width = "400px"; 
+        container.style.width = "400px";
+        container.style.minHeight = '175px';
         container.style.padding = "0px";
         container.style.border = 'solid 2px #f56b2a'; 
         container.style.top = window.scrollY+10+"px"; 
@@ -135,7 +143,6 @@ links.filter(testLinkToLBC).forEach(function(link) {
                     emptyElement(container);
                     var texte = response.responseText; 
                     var adviewDisabled = /adviewDisabled/.test(texte); 
-                    var html =''; 
                     if (adviewDisabled) {
                         var inactive = document.createElement('h1');
                         inactive.style.textAlign = 'center';
@@ -162,7 +169,7 @@ links.filter(testLinkToLBC).forEach(function(link) {
      
     link.addEventListener('mouseout',function() { 
         if(container) { 
-            container.parentNode.removeChild(container); 
+           container.parentNode.removeChild(container); 
         } 
     },false); 
 });
